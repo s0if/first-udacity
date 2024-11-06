5d39084928f4332c78e879c57cb9045729567b4d
s0if <saifalkomi@gmail.com>
2024-11-06 12:15:52 +0200

    addJS

---
+++ JS/scripr.js
@@ -0,0 +1,7 @@
+const dashbord=document.querySelector("#dashbord");
+const Button=document.querySelector("#list");
+Button.onclick=() =>{
+    // dashbord.style.display="none"?"block":"none";
+    dashbord.classList.toggle("hidden");
+}
+
